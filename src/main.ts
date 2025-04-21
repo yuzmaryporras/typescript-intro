19292960894a2f1ff2c6c310716106148cd2e427
yuzmaryporras <yuzmaporras@gmail.com>
2025-04-21 13:53:41 -0400

    feat: creation and use of decorators

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import { name } from './bases/01-types.ts'
-import { charmander } from './bases/03_classes.ts'
+import { charmander } from './bases/05-decorators2.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
